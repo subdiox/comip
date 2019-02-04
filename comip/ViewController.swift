c2beb4b7617dd1fca4eb41fa1385459d8d3d93e6
subdiox <subdiox@gmail.com>
2019-02-04 19:17:44 +0900

    Initial Commit

---
+++ comip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  comip
+//
+//  Created by subdiox on 2019/02/04.
+//  Copyright © 2019 Yuta Ooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
